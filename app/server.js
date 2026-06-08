7799867b702cf44666f0be2be0b0a0c023ad93c2
sanjay108ideaspace <sanjay@108ideaspace.com>
2026-06-08 14:32:56 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
     if(req.url == "/about"){
     res.end('Welcome to our page');
     }else{
-    res.end('Testing Pipeline Test 1');
+    res.end('Testing Pipeline Test 2');
     }
 });
 
