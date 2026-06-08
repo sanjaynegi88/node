2821a67f178328e0970d1110143a9d394b4cec53
sanjay108ideaspace <sanjay@108ideaspace.com>
2026-06-08 13:28:18 +0530

    Adding Compose for docker

---
+++ app/server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    if(req.url == "/about"){
+    res.end('Welcome to our page');
+    }else{
+    res.end('Hello World from Node.js Docker');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
+
+
