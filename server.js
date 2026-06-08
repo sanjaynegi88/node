1d31170627a882948c8365bcaec871db53c1dd5d
Sanjay Singh <phprebels@gmail.com>
2026-06-08 10:46:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,3 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
     console.log('Server running on port 3000');
 });
-
-
-const server = http.createServer((req, res) => {});
